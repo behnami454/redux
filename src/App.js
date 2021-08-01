dcec9a562ebbd1dbf7f3f8104a7fa2d689e1f779
behnami454 <behnami454@gmail.com>
2021-08-01 16:00:14 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment } from './actions/counter'
+import { toggle } from './actions/lightswitch'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+
+  incrementCounter = () => {
+    this.props.increment()
+  }
+
+  toggleLightSwitch = () => {
+    console.log('CHECK YOURSELF <MAN')
+    this.props.toggle()
+  }
+
+  render() {
+    console.log(this.props)
+    const { lightswitch } = this.props
+    
+    const background = lightswitch ? 'yellow' : 'black';
+
+    return (
+      <div className="App" style={{backgroundColor: background} }>
+        <header className="App-header" style={{marginBottom:'10px'}}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">redux</h1>
+        </header>
+        <p className="App-intro">
+         <code></code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>{ this.props.counter } </h1>
+        <button onClick={this.incrementCounter}>+</button> 
+        <button onClick={this.toggleLightSwitch}>light</button> 
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return { 
+    counter: state.counter,
+    lightswitch: state.lightswitch
+  }
+}
+
+export default connect(mapStateToProps, { increment, toggle })(App)
